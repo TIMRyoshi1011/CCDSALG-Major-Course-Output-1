5567efbae31a684207365d103bd798b98d56b735
To iu mono Ryōshi <marcustimothyr1@gmail.com>
2025-07-05 02:34:57 +0800

    Task 2.1: Header file for slow sort

---
+++ sort.h
@@ -0,0 +1,14 @@
+// Author: Marcus Timothy V. Ramos, S12
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+typedef struct { // Points structure to hold coordinates for the stack
+    double x;
+    double y;
+} Point;
+
+// Function prototypes for sorting operations
+double polarAngle(Point p0, Point p);
+Point anchorPoint(Point *points, int n);
+void bubbleSort(Point *points, int n);
