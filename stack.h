2c55b7c515025899678432645f9a7eeeb337f942
To iu mono Ryōshi <marcustimothyr1@gmail.com>
2025-07-05 01:21:56 +0800

    Task 1: Header file for stack

--- stack.h
+++ stack.h
@@ -1,19 +1,24 @@
 // Author: Marcus Timothy V. Ramos, S12
-
 #include <stdio.h>
 #include <stdlib.h>
 
 #define MAX 32768
 
-struct Stack {
-    double points[MAX];
+typedef struct { // Points structure to hold coordinates for the stack
+    double a;
+    double b;
+} Points;
+
+struct Stack { // Stack structure to hold Points and the top index
+    Points points[MAX];
     int top;
 };
 
+// Function prototypes for stack operations
 void create(struct Stack *S);
 int isFull(struct Stack *S);
 int isEmpty(struct Stack *S);
-void push(struct Stack *S, double elem);
-double pop(struct Stack *S);
-double top(struct Stack *S);
-double nextToTop(struct Stack *S);
+void push(struct Stack *S, Points elem);
+Points pop(struct Stack *S);
+Points top(struct Stack *S);
+Points nextToTop(struct Stack *S);
