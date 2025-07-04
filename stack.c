82f57bb5483dcd5ad9a462f5c064708c53dc185d
To iu mono Ryōshi <marcustimothyr1@gmail.com>
2025-07-05 01:23:25 +0800

    Task 1: Functions file for stack

--- stack.c
+++ stack.c
@@ -1,5 +1,4 @@
 // Author: Marcus Timothy V. Ramos, S12
-
 #include "stack.h"
 
 void create(struct Stack *S) {
@@ -14,47 +13,45 @@ int isEmpty(struct Stack *S) {
     return S->top == -1;
 }
 
-void push(struct Stack *S, double elem) {
+void push(struct Stack *S, Points elem) {
     if (!isFull(S)) {
         (S->top)++;
         S->points[S->top] = elem; 
     }
 
     else
-        printf("Stack Overflow, Cannot push %.1lf element\n", elem);
+        printf("Stack Overflow\n");
 }
 
-double pop(struct Stack *S) {
+Points pop(struct Stack *S) {
     if (!isEmpty(S)) {
-        double temp = S->points[S->top];
+        Points temp = S->points[S->top];
         S->top--;
         return temp;
     }
 
     else {
         printf("Stack Underflow\n");
-        return -1; 
+        return (Points){-1, -1}; 
     }
 }
 
-double top(struct Stack *S) {
+Points top(struct Stack *S) {
     if (!isEmpty(S)) {
         return S->points[S->top]; 
     }
 
     else {
-        printf("Stack is empty. No top element.\n");
-        return -1; 
+        return (Points){-1, -1}; 
     }
 }
 
-double nextToTop(struct Stack *S) {
+Points nextToTop(struct Stack *S) {
     if (S->top >= 1) {
         return S->points[S->top - 1];
     }
     
     else {
-        printf("Not enough elements in the stack. No next-to-top element.\n");
-        return -1; 
+        return (Points){-1, -1}; 
     }
 }
